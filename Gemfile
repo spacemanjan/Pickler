69da1a447979b199fd8778756027b0962cd7f101
Yan maurice <yanmcniven@gmail.com>
2016-09-07 23:03:27 +0000

    add project to Heroku

--- Gemfile
+++ Gemfile
@@ -15,4 +15,9 @@ group :development, :test do
   gem 'pry'
   gem 'shotgun'
   gem 'sqlite3'
+
+end
+group :production do
+  gem 'rails_12factor'
+  gem 'pg'
 end
--- app/actions.rb
+++ app/actions.rb
@@ -86,4 +86,32 @@ post '/signup' do
     else
         erb(:signup)
     end
+
+    post '/comments' do
+        params.to_s
+    end
+    
+    post '/comments' do
+        text = params[:text]
+        post_id = params[:post_id]
+        
+        comment = Comment.new({ text: text, post_id: post_id, user_id: current_user.id})
+        
+        comment.save
+        
+        redirect(back)
+    end
+    post '/likes' do 
+        post_id = params[:post_id]
+        
+        like = Like.new({ post_id: post_id, user_id: current_user.id })
+        like.save
+        
+        redirect(back)
+    end
+    delete '/likes/:id' do
+        like = Like.find(params[:id])
+        like.destroy
+        redirect(back)
+    end
 end
